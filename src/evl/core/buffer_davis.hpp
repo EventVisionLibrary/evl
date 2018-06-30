66c9faa2a2c233bcb4ea31f472463eb9fc1a5a6e
Shintaro Shiba <shiba.shintaro@gmail.com>
2018-06-30 16:06:57 +0900

    modified include guard

--- src/evl/core/buffer_davis.hpp
+++ src/evl/core/buffer_davis.hpp
@@ -1,9 +1,9 @@
 // Copyright 2018 Event Vision Library.
-#ifndef SRC_EVL_CORE_READ_DAVIS_HPP_
-#define SRC_EVL_CORE_READ_DAVIS_HPP_
+#ifndef SRC_EVL_CORE_BUFFER_DAVIS_HPP_
+#define SRC_EVL_CORE_BUFFER_DAVIS_HPP_
 
 #include "types.hpp"
 
 void bufferData(EventBuffer *buffer);
 
-#endif  // SRC_EVL_CORE_READ_DAVIS_HPP_
+#endif  // SRC_EVL_CORE_BUFFER_DAVIS_HPP_
--- src/evl/core/read_buffer.hpp
+++ src/evl/core/read_buffer.hpp
@@ -1,6 +1,6 @@
 // Copyright 2018 Event Vision Library.
-#ifndef SRC_EVL_CORE_DAVISIO_HPP_
-#define SRC_EVL_CORE_DAVISIO_HPP_
+#ifndef SRC_EVL_CORE_READ_BUFFER_HPP_
+#define SRC_EVL_CORE_READ_BUFFER_HPP_
 
 #include <vector>
 #include "types.hpp"
@@ -13,4 +13,4 @@ std::vector<EventTuple> readBufferOnNumber(EventBuffer *buffer, int number);
 
 void loop_readData(EventBuffer *buffer, int lifetime);
 
-#endif  // SRC_EVL_CORE_DAVISIO_HPP_
+#endif  // SRC_EVL_CORE_READ_BUFFER_HPP_
