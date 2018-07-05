d3af3a33745959df9bea4d899a66b7bd7d78cbf2
riku arakawa <a_ra_ra_rikky0611@yahoo.co.jp>
2018-07-05 23:37:51 +0900

    Update buffer_csv and buffer_davis to use utils

--- examples/buffer_davis/main.cpp
+++ examples/buffer_davis/main.cpp
@@ -1,18 +1,45 @@
 // Copyright 2018 Event Vision Library.
+#include <iostream>
 #include <thread>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
 #include <evl/core/types.hpp>
 #include <evl/core/buffer_davis.hpp>
 #include <evl/core/read_buffer.hpp>
+#include <evl/utils/event_utils.hpp>
+
+void loop_readData(EventBuffer *buffer, int lifetime) {
+    usleep(100000);      // micro sec
+    while (1) {
+        usleep(100000);      // micro sec. calling frequency
+        std::vector<EventTuple> v = readBufferOnLifetime(buffer, lifetime);
+
+        std::cout << "[Thread2] DATA READING =============" << std::endl;
+        std::cout << "[Thread2] Lifetime >>> " << lifetime << std::endl;
+        std::for_each((v).begin(), (v).end(), disp_event);
+        std::cout << "[Thread2] DATA READ DONE ===========" << std::endl;
+    }
+}
+
+void loop_readImage(EventBuffer *buffer, int lifetime) {
+    cv::namedWindow("image", 1);
+    usleep(100000);      // micro sec
+    while (1) {
+        usleep(100000);      // micro sec. calling frequency
+        std::vector<EventTuple> v = readBufferOnLifetime(buffer, lifetime);
+        cv::Mat img = events_to_mat(v);
+        cv::imshow("image", img);
+        cv::waitKey(1);
+    }
+}
 
 int main() {
     int lifetime = 2000;     // micro sec
     int buffersize = 50000;
     EventBuffer buffer(buffersize);
-
     std::thread t1(bufferData, &buffer);
-    std::thread t2(loop_readData, &buffer, lifetime);
-
+    loop_readData(&buffer, lifetime);
     t1.join();
-    t2.join();
     return 0;
 }
