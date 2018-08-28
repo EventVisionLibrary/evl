741c11dc9b0cded7c5de7acd3dc8aeba1a6d2997
Riku Arakawa <a_ra_ra_rikky0611@yahoo.co.jp>
2018-08-28 16:05:55 +0900

    Change file name

--- examples/save_csv/main.cpp
+++ examples/save_csv/main.cpp
@@ -1,19 +1,18 @@
 // Copyright 2018 Event Vision Library.
 #include <thread>
-
-#include <evl/core/save_csv.hpp>
-#include <evl/core/store_buffer.hpp>
 #include <evl/core/types.hpp>
+#include <evl/core/store_buffer.hpp>
+#include <evl/core/save_csv.hpp>
 
 int main() {
     int lifetime = 1000;     // micro sec
     int buffersize = 50000;
     evl::EventBuffer buffer(buffersize);
-    char orgFname[] = "../../data/7.csv";
-    std::string newFname = "test.txt";
+    char original_file[] = "../../data/sample.csv";
+    std::string new_file = "../../data/test.csv";
 
-    std::thread t1(evl::storeBufferFromCsv, &buffer, orgFname);
-    std::thread t2(evl::saveBuffer, &buffer, lifetime, newFname);
+    std::thread t1(evl::storeBufferFromCsv, &buffer, original_file);
+    std::thread t2(evl::saveBuffer, &buffer, lifetime, new_file);
 
     t1.join();
     t2.join();
