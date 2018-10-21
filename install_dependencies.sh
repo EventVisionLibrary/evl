54417cbc9e54dd535b70cf54baa523d3c0292224
rikky0611 <a_ra_ra_rikky0611@yahoo.co.jp>
2018-10-21 04:31:35 +0000

    Modify

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,6 +1,7 @@
 # libcaer
 git clone https://github.com/inilabs/libcaer.git
 cd libcaer
+git reset --hard b77bc2b54f45efd8f8b6ef05074bf06491c33e3b
 mkdir build
 cd build
 cmake -DCMAKE_INSTALL_PREFIX=/usr ..
